9b514c62425cd286fa3a0b370332639810f65674
Jillian Kovacevich <jkovacevich3@Jillians-MacBook-Pro-2.local>
2015-04-02 17:45:54 -0700

    Chart pages
    Updated the style sheet with various things — kept all originals, but
    commented out if they weren’t need anymore.
    
    Created profile pages for BMI, head, height and weight.

---
+++ profileweight.php
@@ -0,0 +1,85 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="childcheckstyle.css">
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <title>Child Check Weight</title>
+
+    <style></style>
+</head>
+
+<body>
+
+
+
+<div id="profile-container">
+    <div id="banner">
+        <img src="./images/small-logo.png" alt="Child Check"/>
+    </div>
+    <br>
+    <div id="profile-img-container">
+        <div id="profile-img">
+        </div>
+    </div>
+    <div id="navbar">
+        <div class="box">
+            <a href="profileheight.php"><img src="./images/height.png" alt="Height"/></a>
+            <div id="navLabel">Height</div>
+        </div>
+        <div class="whitebox">
+            <a href="profileweight.php"><img src="./images/bmi.png" alt="Weight"/></a>
+            <div id="navLabel">Weight</div>
+        </div>
+        <div class="box">
+            <a href="profilehead.php"><img src="./images/head.png" alt="Head Circumference"/></a>
+            <div id="navLabel">Head Size</div>
+        </div>
+        <div class="box">
+            <a href="profileBMI.php"><img src="./images/bmi.png" alt="BMI"/></a>
+            <div id="navLabel">Body Mass Index (BMI)</div>
+        </div>
+        <div class="box">
+            <a href="#"><img src="./images/head.png" alt="Milestones"/></a>
+            <div id="navLabel">Milestones</div>
+        </div></div>
+    <div style="clear: both;"></div>
+    <br>
+    <div id="info">
+
+        <div class="info-half">
+            <div id="header3">WEIGHT REPORT</div>
+            <div id="stats">Current Height:
+                <div id="currentNumber">30
+                    <div id="currentUnit">in.</div></div>
+                <div id="asOf">as of February 20, 2015</div></div>
+
+            <div id="statsLast">
+                From Your Last Visit:
+                <div id="currentNumber">30
+                    <div id="currentUnit">in.</div></div>
+                <div id= "viewPast">View all past assessments</div></div>
+
+
+            <div id="chartInfo">Compared to her last assessment results, Samantha is growing at a healthy rate!<br/>
+                <p style="margin-left: 20%; text-align: right;">Here is a list of healthy foods that physicians recommend for children under the age of 18.</p></div>
+        </div>
+
+        <div class="info-half2">
+            <div id="chart">CHART WILL GO HERE</div>
+        </div>
+        <div style="clear: both;"></div>
+
+
+    </div>
+    <div id="footer">
+        &copy; 2015 ITP-460 & USC D-Health. All Rights Reserved.
+    </div>
+</div>
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/bootstrap.js"></script>
+
+</body>
+</div>
+</html>
