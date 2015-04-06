79b92c9ed70fc27f22bd282a683d98a11af8d885
Benjamin Dunn <benjamindunn925@gmail.com>
2015-04-05 18:20:05 -0700

    Password checking
    Checks password

--- signup.php
+++ signup.php
@@ -34,6 +34,69 @@
             }
             alert(values);
         }
+
+        function passwordChecker(pw){
+            if(pw.length<8){
+                alert("Your password must be 8 characters or more.");
+            }
+            if (pw == pw.toUpperCase() || pw == pw.toLowerCase()){
+                alert("Your password must have upper and lower case characters.");
+            }
+            var looking1 = "!";
+            var looking2 = "@";
+            var looking3 = "#";
+            var looking4 = "$";
+            var looking5 = "%";
+            var looking6 = "^";
+            var looking7 = "&";
+            var looking8 = "*";
+            var looking9 = "(";
+            var looking0 = ")";
+            var tracking= 0;
+
+            if (pw.indexOf(looking1) >0) {
+                tracking = tracking +1;
+            }
+            if (pw.indexOf(looking2) >0) {
+                tracking = tracking +1;
+            }
+            if (pw.indexOf(looking3) >0) {
+                tracking = tracking +1;
+            }
+            if (pw.indexOf(looking4) >0) {
+                tracking = tracking +1;
+            }
+            if (pw.indexOf(looking5) >0) {
+                tracking = tracking +1;
+            }
+            if (pw.indexOf(looking6) >0) {
+                tracking = tracking +1;
+            }
+            if (pw.indexOf(looking7) >0) {
+                tracking = tracking +1;
+            }
+            if (pw.indexOf(looking8) >0) {
+                tracking = tracking +1;
+            }
+            if (pw.indexOf(looking8) >0) {
+                tracking = tracking +1;
+            }
+            if (pw.indexOf(looking9) >0) {
+                tracking = tracking +1;
+            }
+            if(tracking ==0){
+                alert("Your password must contain a special character");
+            }
+
+
+
+
+
+
+
+
+
+        }
     </script>
     <style>
         .page{
@@ -68,6 +131,7 @@
 
     <div style="margin-left: auto; margin-right: auto;">
 
+<<<<<<< Updated upstream
         <form id="multiForm" method="post" action="create_login.php">
             <div id="page1" class="page" style="visibility:visible;">
                 ACCOUNT SETUP
@@ -77,6 +141,21 @@
                 <p><input type="submit" id="C1" value="Continue" ></p>
             </div>
         </form>
+=======
+    <form id="multiForm" method="post" action="add_child.php">
+        <div id="page1" class="page" style="visibility:visible;">
+
+            ACCOUNT SETUP
+            <p><label>PLEASE ENTER A USERNAME</label> <input type="text" id="username" size="20" maxlength="20px" style="width: 350px;"></p>
+            <p><label>PASSWORD </label><input type="password" id="password"  style="width: 350px;"  onchange="passwordChecker(this.value)" ></p> <!// must be 8 charactes, must include numbers and symbols//!>
+            <p><label>CONFIRM PASSWORD </label> <input type="password" id="passwordConfirm" style="width: 350px;"></p>
+            <p><input type="submit" id="C1" value="Continue" ></p>
+
+        </div>
+
+
+    </form>
+>>>>>>> Stashed changes
 
     </div>
 
