778336bc2f1da948e55fe8b33d26a0cd3759389b
ahheelin <kimailinj@gmail.com>
2015-04-26 20:33:31 -0700

    Footer fixed

--- faq/index.php
+++ faq/index.php
@@ -58,50 +58,42 @@
 
     <div id="question">
         How can you tell the difference between a child who is just taking his or her time and one who has a true developmental delay?</div>
-    <div id="answer">Developmental delay is when a child does not reach a milestone by the upper range of what is considered healthy. Even though babies develop at their own pace, every child should do certain tasks by a certain age. A child can stray from the timeline and still be within the range of healthy development, but it's best to discuss any concerns with your child's pediatrician</div>
+    <div id="answer">Developmental delay is when a child does not reach a milestone by the upper range of normal. Even though babies develop at their own pace, he explains, "every child should do certain tasks by a certain age.”</div>
+    <div id="answer">A child can stray from the timeline and still be within the range of normal, but it's best to discuss any concerns with your pediatrician</div>
     <br/><div class="line"></div><br/>
 
-    
+    <div id="question">
+        How can you tell the difference between a child who is just taking his or her time and one who has a true developmental delay?</div>
+    <div id="answer">Developmental delay is when a child does not reach a milestone by the upper range of normal. Even though babies develop at their own pace, he explains, "every child should do certain tasks by a certain age.”</div>
+    <br/><div class="line"></div><br/>
+
+    <div id="question">
+        How can you tell the difference between a child who is just taking his or her time and one who has a true developmental delay?</div>
+    <div id="answer">Developmental delay is when a child does not reach a milestone by the upper range of normal. Even though babies develop at their own pace, he explains, "every child should do certain tasks by a certain age.”</div>
+    <br/><div class="line"></div><br/>
+
+
     <div id="question">
         Why hasn't my child reached a certain milestone yet?</div>
+
     <div id="subquest">Have you given them a chance?</div>
-    <div id="answer">"Often there's not even a delay. Sometimes a parent just isn't given the child an opportunity. For example, a baby may not sit on his own because he's always being held, rather than having time on the floor."</div>
+    <div id="answer">"Often there's not even a delay. Sometimes a parent just isn't giving the child opportunities. For example, a baby may not sit alone because he's always being held, rather than having time on the floor."</div>
 
     <div id="subquest">Was your child born premature?</div>
-    <div id="answer">"Children who are premature may not have the same rate of muscle strength and development, so keep this is mind when you're evaluating his or her progress.</div>
+    <div id="answer">"Children who are premature may not have the same rate of muscle strength and development,"</div>
 
-    <div id="subquest">Is the delay speech or comprehension related?</div>
-    <div id="answer">If so, likely culprit is hearing loss due to recurrent ear infections.  A less common cause is autism, particularly if the child also has difficultly interacting socially</div>
+    <div id="subquest">Speech/comprehension related?</div>
+    <div id="answer">Likely culprit is hearing loss due to recurrent ear infections</div>
+    <div id="subquest">A less common cause is autism, particularly if the child also has difficult interacting socially</div>
 
-    <div id="subquest">Is your child experiencing a truly significant delay?</div>
-    <div id="answer">This is a serious issue that could be attributed to genetic disorders such as Down syndrome or developmental disablities like cerbral palsy or mental retardation.</div>
-
-    <div id="question">How common are significant delays?</div>
-    <div id="answer"> Significant developmental delays affect approximately 2% of US children.</div>
-
-    <div id="question">If my child is experiencing a delay, what can be done to help get them back on track?</div>
-    <div id="answer"> There are a range of options when it comes to helping your child progress in a healthy way.  Some include occupational therapy, physical therapy, speech therapy, and special education programs.</div>
-
-<div id="question">
-        If I think something is wrong, who should I contact?</div>
-    <div id="answer">For all questions and concerns, reach out to your child's pediatrician.</div><br/>
-    <br/><div class="line"></div><br/>
-
-<div id="question">
-        How can I see my child's growth compared to the national average?</div>
-    <div id="answer">There is a toggle switch above each growth graph that will allow you to turn the national average chart on or off.</div>
-    <br/><div class="line"></div><br/>
-
-<div id="question">
-        Does Child Check use the CDC's growth charts?</div>
-    <div id="answer">Yes.</div>
-    <br/><div class="line"></div><br/>
+    <div id="subquest">Significant delay?</div>
+    <div id="answer">Genetic disorders such as down syndrome and developmental disabilities such as cerebral palsy or mental retardation.</div>
 
 </div>
 
 </div>
     <br>
-    <div id="footer">&copy; 2015 ITP-460 & USC D-Health. All Rights Reserved.</div>
+    <div id="footer" style="width: 100%;">&copy; 2015 ITP-460 & USC D-Health. All Rights Reserved.</div>
 </div>
 	
 	<script src="../js/jquery-2.1.1.js"></script>
@@ -133,4 +125,8 @@
 	</script>
 </body>
 
-
+<!--
+Link mit button to home page
+Have not added any bootstrap elements yet
+Need to upload fonts - match size and color to style guide
+Figure out when uploading child picture, that they are evenly spaced-->
