dd825d4b2a48d170c9e8d6123b9a240c2cfa37e7
ahheelin <kimailinj@gmail.com>
2015-04-26 20:27:43 -0700

    Poppet navbar added to about and FAQ

--- faq/index.php
+++ faq/index.php
@@ -6,6 +6,9 @@
     <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css">
     <link rel="icon" href="../images/favicon.ico">
     <link rel="stylesheet" type="text/css" href="../childcheckstyle.css">
+        
+    <link href='http://fonts.googleapis.com/css?family=Roboto:100,700,400' rel='stylesheet' type='text/css'>
+    <script src="../js/modernizr.js"></script>
 
     <title>Child Check FAQ</title>
 
@@ -15,11 +18,36 @@
 
 <body>
 <div class="cd-header">
-    <a href="../">
-        <div class="logo">
-            <img src="../images/small-logo.png" alt="Child Check"/></a>
+	<a href="../"><div class="logo"><img src="../images/small-logo.png" alt="Child Check"/></div></a>
+
+	<div class="nav-container">
+		<ul class="cd-secondary-nav"><li><a href="../process_logout.php">Log Out</a></li></ul>
+	</div>
+
+	<a class="cd-primary-nav-trigger" href="#0">
+		<span class="cd-menu-text">Menu</span><span class="cd-menu-icon"></span>
+	</a>
 </div>
-</div><br/><br/>
+<br>
+<div>
+	<ul class="cd-primary-nav">
+		<li class="cd-label">Children</li>
+		<div id="children-links"></div>
+		<script type="text/handlebars" id="children-template">
+			<li><a href="../profile?id={{mrn}}&last_name={{last_name}}">{{first_name}} {{last_name}}</a></li>
+		</script>
+
+		<li class="cd-label">Settings</li>
+
+		<li><a href="#0">Account Settings</a></li>
+
+
+		<li class="cd-label">Need Help?</li>
+
+		<li><a href="../faq">FAQ</a></li>
+		<li><a href="../about">About ChildCheck</a></li>
+	</ul>
+</div>
 
 
 <div id="containerQuest">
@@ -72,8 +100,37 @@
 </div>
 
 </div>
-<script src="../js/jquery-2.1.1.js"></script>
-<script src="../bootstrap/js/bootstrap.min.js"></script>
+    <br>
+    <div id="footer">&copy; 2015 ITP-460 & USC D-Health. All Rights Reserved.</div>
+</div>
+	
+	<script src="../js/jquery-2.1.1.js"></script>
+	<script src="../js/main.js"></script>
+        <script src="../bootstrap/js/bootstrap.min.js"></script>
+	<script>
+	$( document ).ready(function() {
+		$(window).on('scroll',
+			{
+			    previousTop: 0
+			}, 
+			function () {
+			    var currentTop = $(window).scrollTop();
+			    //check if user is scrolling up
+			    if (currentTop < this.previousTop ) {
+				//if scrolling up...
+				//add class 'is-visible' to the main navigation
+				//if currentTop == 0, remove 'is-fixed' and 'is-visible' classes 
+			    } else {
+				//if scrolling down...
+				//add the 'is-fixed' class to the main navigation as soon as it is no longer visible
+				//add class 'is-visible' to the main navigation
+			    }
+			    //set previousTop for the next iteration
+			    this.previousTop = currentTop;
+			}
+		);
+	});
+	</script>
 </body>
 
 
