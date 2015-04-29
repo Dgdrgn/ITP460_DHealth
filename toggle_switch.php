71f29f343de1e2631f99e3b2bc38d6672411fce4
Benjamin Dunn <benjamindunn925@gmail.com>
2015-04-29 00:33:16 -0700

    Toggle Switching!
    Works between two line graphs included in this file :D

--- toggle_switch.php
+++ toggle_switch.php
@@ -13,16 +13,32 @@
 
 </script>
 <body>
-<!-- line chart canvas element -->
+<!-- line chart canvases -->
 <input type="button" value="toggle" onclick="hide()">
-<canvas id="buyers" width="1000" height="500"></canvas>
-<!-- pie chart canvas element -->
-<canvas id="countries" width="600" height="400"></canvas>
-<!-- bar chart canvas element -->
-<canvas id="income" width="600" height="400"></canvas>
+<canvas id="buyers" width="1000" height="500" style="position:absolute; float: left;"></canvas>
+<canvas id="buyers2" width="1000" height="500" style="visibility: hidden; position: absolute; float: left;"></canvas>
 <script>
-    // line chart dat
+    // line chart1 data
     var buyerData = {
+        labels : ["January","February","March","April","May","June","July","August","September","October","November","December"],
+        datasets : [
+            {
+                fillColor : "rgba(233,130,51,.35)", //orange
+                strokeColor : "rgba(15,119,170,1)", //light blue
+                pointColor : "rgba(9,53,90,.8)", //dark blue
+                pointStrokeColor : "rgba(251,176,61,1)", //light orange
+                data : [203,156,99,251,305,247,300,90,55,156,250,500]
+            }
+        ]
+
+    }
+    // get line chart canvas
+    var buyers = document.getElementById('buyers').getContext('2d');
+    // draw line chart
+    new Chart(buyers).Line(buyerData,  {pointDotStrokeWidth : 1, pointDotRadius : 6, bezierCurveTension :.5,  scaleGridLineColor : "rgba(0,0,0,.15)"} );
+
+    // avg line chart data
+    var buyerData2 = {
         labels : ["January","February","March","April","May","June","July","August","September","October","November","December"],
         datasets : [
             {
@@ -43,10 +59,11 @@
 
     }
     // get line chart canvas
-    var buyers = document.getElementById('buyers').getContext('2d');
+    var buyers2 = document.getElementById('buyers2').getContext('2d');
     // draw line chart
-    new Chart(buyers).Line(buyerData,  {pointDotStrokeWidth : 1, pointDotRadius : 6, bezierCurveTension :.5,  scaleGridLineColor : "rgba(0,0,0,.15)"} );
+    new Chart(buyers2).Line(buyerData2,  {pointDotStrokeWidth : 1, pointDotRadius : 6, bezierCurveTension :.5,  scaleGridLineColor : "rgba(0,0,0,.15)"} );
 
+    //hiding function
     function hide(){
             if (document.getElementById('buyers').style.visibility == "hidden") {
                 document.getElementById('buyers').style.visibility = "visible";
@@ -54,9 +71,16 @@
             else{
                 document.getElementById('buyers').style.visibility = "hidden";
             }
+        if (document.getElementById('buyers2').style.visibility == "hidden") {
+            document.getElementById('buyers2').style.visibility = "visible";
+        }
+        else{
+            document.getElementById('buyers2').style.visibility = "hidden";
+        }
 
 
         }
+
     </script>
 
     </body>
